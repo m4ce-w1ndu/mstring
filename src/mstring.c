e95adbcede50ca6a834ffb193eb79966177264d4
arkgrv <ag.novitskiy@icloud.com>
2023-01-03 19:57:41 +0100

    Added implementations for construct and delete

--- src/mstring.c
+++ src/mstring.c
@@ -38,5 +38,55 @@ inline static size_t new_size(size_t sz)
 
 mstring mstr_construct()
 {
+    mstring str = (mstring) malloc(sizeof(struct mstring_t));
+    if (NULL == str) return NULL;
 
+    // Allocate internal storage
+    str->buffer = (char *) malloc(sizeof(char) * INITIAL_CAPACITY);
+
+    // Allocation error check
+    if (NULL == str->buffer) {
+        // Free the object
+        free(str);
+        return NULL;
+    }
+
+    str->buffer[0] = '\0';
+    str->size = 0;
+    str->capacity = INITIAL_CAPACITY;
+
+    return str;
+}
+
+mstring mstr_construct_init(const char init[])
+{
+    mstring str = (mstring) malloc(sizeof(struct mstring_t));
+    if (NULL == str) return NULL;
+
+    // Get the length of the string
+    size_t len = strlen(init);
+    size_t cap = len + 1;
+
+    // Allocate internal storage and copy the string
+    str->buffer = (char *)malloc(sizeof(char) * cap);
+    if (NULL == str->buffer) {
+        free(str);
+        return NULL;
+    }
+
+    memcpy(str->buffer, init, cap * sizeof(char));
+    str->size = len;
+    str->capacity = cap;
+
+    return str;
+}
+
+void mstr_delete(mstring str)
+{
+    if (NULL == str) return;
+    if (NULL != str->buffer) {
+        free(str->buffer);
+    }
+
+    free(str);
 }
