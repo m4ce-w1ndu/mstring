b583eac7264d7c5e924bf2738e0d774acb8ba98f
arkgrv <ag.novitskiy@icloud.com>
2023-01-03 19:44:24 +0100

    Added string new capacity calculation

--- src/mstring.c
+++ src/mstring.c
@@ -1,2 +1,40 @@
-#include "mstring.h"
+#include "include/mstring.h"
 
+#include <string.h>
+#include <stdlib.h>
+
+/// @brief Contains the actual mstring private
+/// implementation.
+struct mstring_t {
+
+};
+
+mstring mstr_construct()
+{
+
+}
+
+/**
+ * @brief Calculates the new size of the string
+ * 
+ * @param sz 
+ * @return size_t 
+ */
+inline static size_t new_size(size_t sz)
+{
+    --sz;
+
+    sz |= sz >> 1;
+    sz |= sz >> 2;
+    sz |= sz >> 4;
+    sz |= sz >> 8;
+#if defined(ARCH_BITS_32)
+    sz |= sz >> 16;
+#elif defined(ARCH_BITS_64)
+    sz |= sz >> 16;
+    sz |= sz >> 32;
+#endif
+
+    ++sz;
+    return sz;
+}
